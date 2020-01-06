3dae8800a64ce4990c0f45a0151a1acaf5204acf
SqueezyDough <lvanbiljouw@hotmail.com>
2020-01-06 15:46:11 +0100

    Add map, restructure project

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app
+  .use('/static', express.static(path.join(__dirname, 'static')))
+  .set('view engine', 'hbs')
+  .engine('hbs', exphbs({
+    defaultLayout: 'main',
+    partialsDir: path.join(__dirname, 'views/partials'),
+    extname: '.hbs'
+  }))
+  .get('/', (req, res) => {
+    res.render('home');
+  })
+  .listen(port, () => console.log(`App listening on port ${port}!`));
